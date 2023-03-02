f37e3a58bc3ad0f7a8a9e38d53501ec5a313e617
Abhishek-90 <holaniabhishek90@gmail.com>
2023-03-02 18:45:01 +0530

    container added

--- src/ITubeApp.tsx
+++ src/ITubeApp.tsx
@@ -3,20 +3,26 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import VideoSearchResult from "./pages/VideoSearchResult";
 import VideoStream from "./pages/VideoStream";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import styled from "styled-components";
 
 function ITubeApp() {
   return (
-    <>
+    <AppContainer>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/video-search-result/:query"
-          element={<VideoSearchResult />}
-        />
-        <Route path="/video-stream/:id" element={<VideoStream />} />
+        <Route path="/result" element={<VideoSearchResult />} />
+        <Route path="/stream" element={<VideoStream />} />
       </Routes>
-    </>
+      <Footer />
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  width: 100vh;
+`;
 
 export default ITubeApp;
