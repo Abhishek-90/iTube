7abb81f06fc9f607c84d405be323ef0fc3cd77e0
Abhishek-90 <holaniabhishek90@gmail.com>
2023-03-03 16:00:11 +0530

    constants added

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,3 @@
+export const rapidApi: string =
+  "https://youtube-search-and-download.p.rapidapi.com";
+export const search: string = "search";
---
+++ src/constants/fetchOptions.ts
@@ -0,0 +1,7 @@
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
