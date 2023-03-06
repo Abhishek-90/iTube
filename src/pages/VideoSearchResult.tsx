1544a69134c7641b722a868cb4423ac18e9f40db
Abhishek-90 <holaniabhishek90@gmail.com>
2023-03-07 00:34:59 +0530

    video search component started

--- src/pages/VideoSearchResult.tsx
+++ src/pages/VideoSearchResult.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import styled from "styled-components";
+import appFetch from "../utils/AppFetch";
+import * as O from "../constants/fetchOptions";
+import * as E from "../constants/endpoints";
+import VideoItem from "../components/VideoItem";
 
 function VideoSearchResult() {
-  return <div>VideoSearchResult</div>;
+  const [videoData, setVideoData] = useState<any>([]);
+  const [params, setParams] = useSearchParams();
+
+  useEffect(() => {
+    async function getData() {
+      const response = await appFetch(
+        `${E.rapidApi}/${E.search}?query=${params.get("query")}`,
+        O.searchOptions
+      );
+      setVideoData(response.contents);
+      console.log(videoData);
+    }
+    getData();
+  }, [params]);
+
+  return (
+    <Container>
+      <Content>
+        {videoData.length > 0 &&
+          videoData.map((videoData: any) => (
+            <VideoItem videoData={videoData.video} />
+          ))}
+      </Content>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 90%;
+  max-width: 1440px;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default VideoSearchResult;
